97e8081755a7afbcb0686a6911fb1f2af867527d
zhangchenhao7 <chenhaozhang@Chenhaos-MacBook-Pro.local>
2016-05-30 15:21:07 -0500

    Made console program continuous

--- todoMVVM1/src/display/TodoApp.java
+++ todoMVVM1/src/display/TodoApp.java
@@ -13,19 +13,26 @@ public class TodoApp {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("To create a user, press 1; to log in, press 2");
-        String choice = scanner.next();
-        System.out.println("Please enter your username:");
-        String userName = scanner.next();
-        System.out.println("Please enter your password:");
-        String password = scanner.next();
         List<UserAccount> users = new LinkedList<>();
         UserCtrlr userCtrlr = new UserCtrlr(users);
+        userCtrlr.addAUser("default", "default");
+        while(true) {
+            System.out.println("\nTo create a user, press 1; to log in, press 2");
+            String choice = scanner.next();
+            if (choice.equals("2")) { //choice == "2" didn't return true
+                System.out.println("Use username 'default' and password 'default' to log in.");
+            }
+            System.out.println("Please enter your username:");
+            String userName = scanner.next();
+            System.out.println("Please enter your password:");
+            String password = scanner.next();
 
-        if (choice.equals("1")) { //choice == "1" didn't return true
-            userCtrlr.addAUser(userName, password);
-        } else {
+            if (choice.equals("1")){
+                userCtrlr.addAUser(userName, password);
+            }
+
             userCtrlr.showAUser(userName, password);
+
         }
 
     }
